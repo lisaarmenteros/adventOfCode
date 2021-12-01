ee272244cb1ebeaabb3445a4ba3d0f4470770bf2
Lisa Armenteros <lisaarmenteros@gmail.com>
2021-12-01 02:43:45 -0500

    Improved code for Day 1

--- 2021/d1p1_solution.py
+++ 2021/d1p1_solution.py
@@ -4,14 +4,9 @@ inputFile = open ('d1p1_input.txt','r')
 
 Lines = inputFile.readlines()
 count = 0
-numlines=0
-
-# Count the number of lines in file 
-for line in Lines:
-    numlines+=1
 
 # Iterrate through the lines and count which lines increment from the previous line
-for i in range (numlines-1):
+for i in range (len(Lines)-1):
     if int(Lines[i]) < int(Lines[i + 1]):
         count+=1
 
