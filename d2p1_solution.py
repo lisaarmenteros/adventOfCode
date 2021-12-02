13123f8c8aa84451cc6ae3e4df7b86424b33e444
Lisa Armenteros <lisaarmenteros@gmail.com>
2021-12-02 10:27:03 -0500

    first commit

---
+++ d2p1_solution.py
@@ -0,0 +1,28 @@
+# Advent of Code - Day 2 Problem 1
+
+inputFile = open ('d2_input.txt','r')
+
+Lines = inputFile.readlines()
+depth=0
+horzpos=0
+
+
+
+for i in range (len(Lines)-1):
+    instruction = Lines[i].split()
+    if(instruction[0] == "forward"):
+        horzpos += int(instruction[1])
+        print("Moving forward!")
+        print("horpos:", horzpos)
+    elif (instruction[0] == "down"):
+        depth += int(instruction[1])
+        print("Moving down!")
+        print("depth:", depth)
+    elif (instruction[0] == "up"):
+        depth -= int(instruction[1])
+        print("Moving up!")
+        print("depth:", depth)
+
+print(depth * horzpos)
+
+inputFile.close()
