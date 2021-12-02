6b617bccd2628374c73f24b31e9dd91a7444dd3c
Lisa Armenteros <lisaarmenteros@gmail.com>
2021-12-02 10:48:45 -0500

    Problem 2 Complete

--- 2021/d2p2_solution.py
+++ 2021/d2p2_solution.py
@@ -1,1 +1,22 @@
 # Advent of Code - Day 2 Problem 2
+
+inputFile = open ('d2_input.txt','r')
+
+Lines = inputFile.readlines()
+depth=0
+horzpos=0
+aim=0
+
+for i in range (len(Lines)):
+    instruction = Lines[i].split()
+    if(instruction[0] == "forward"):
+        horzpos += int(instruction[1])
+        depth += (aim * int(instruction[1]))
+    elif (instruction[0] == "down"):
+        aim += int(instruction[1])
+    elif (instruction[0] == "up"):
+        aim -= int(instruction[1])
+
+print(depth * horzpos)
+
+inputFile.close()
