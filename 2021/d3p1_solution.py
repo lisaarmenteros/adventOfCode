5cd4fdd642009705e17aa6831afd947a4a3c610a
Lisa Armenteros <lisaarmenteros@gmail.com>
2021-12-03 15:30:34 -0500

    Problem 1 complete

--- 2021/d3p1_solution.py
+++ 2021/d3p1_solution.py
@@ -5,11 +5,11 @@ inputFile = open ('input/d3_input.txt','r')
 
 Lines = inputFile.readlines()
 
-length = len(list(Lines[0]))
+length = len(list(Lines[0])) - 1
 gamma = 0
 epsilon=0
 
-for i in range(length - 1):
+for i in range(length):
     numzeros=0
     numones=0
     for j in range(len(Lines)):
@@ -19,9 +19,9 @@ for i in range(length - 1):
         if binary[i] == "0":
             numzeros += 1 
     if numones > numzeros:
-        gamma += (2 ** (length -2 - i))
+        gamma += (2 ** (length -1 - i))
     if numones < numzeros:
-        epsilon += (2 ** (length -2 - i))
+        epsilon += (2 ** (length -1 - i))
 
 print(gamma * epsilon)
 
