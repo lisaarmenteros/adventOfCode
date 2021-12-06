1ff1e4b874c3f530c1b0d21cb5d5c977368f5eb1
Lisa Armenteros <lisaarmenteros@gmail.com>
2021-12-06 17:03:11 -0500

    Problem 1 complete

--- 2021/d6p1_solution.py
+++ 2021/d6p1_solution.py
@@ -1,1 +1,21 @@
 # Advent of Code - Day 6 Problem 1
+
+inputFile = open ('input/d6_input.txt','r')
+lines = inputFile.readlines()
+lines = lines[0].split(",")
+
+
+# Cicle through 80 days
+for i in range(80):
+    for j in range(len(lines)):
+        # If number == 0, set it = 6 and append 8 to the end of the list
+        if int(lines[j]) == 0:
+            lines[j] = 6
+            lines.append(8)
+        # ElIf number is <=8, set it -= 1
+        elif int(lines[j]) <= 8:
+            lines[j] = int(lines[j]) - 1
+
+print(len(lines))
+
+inputFile.close()
