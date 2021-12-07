e8ce83b6f354037da2bfe32223665b43e5648bf3
Lisa Armenteros <lisaarmenteros@gmail.com>
2021-12-07 03:17:37 -0500

    Problem 1 complete/usr/bin/python3

--- 2021/d7p1_solution.py
+++ 2021/d7p1_solution.py
@@ -1,1 +1,28 @@
 # Advent of Code - Day 7 Problem 1
+
+# Read input 
+inputFile = open ('input/d7_input.txt','r')
+lines = inputFile.readlines()
+lines = list(map(int, lines[0].split(",")))
+
+
+def calculateFuel(list, middle):
+    fuel = 0
+    for i in range(len(list)):
+        fuel += abs(middle - list[i])
+    return fuel
+
+# Sort input
+lines.sort()
+
+# Get two middle numbers in case the number is even and check which provides a cheaper fuel result
+middle = lines[int(len(lines)/2)]
+middle2 = lines[int((len(lines)/2)) - 1]
+
+total = calculateFuel(lines, middle)
+total2 = calculateFuel(lines, middle2)
+
+if total < total2:
+    print(total)
+else:
+    print(total2)
